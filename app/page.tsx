a69874354925d05b4e8776c3cd723c97bd696840
dilaksh06 <kamalathasandilakshan@gmail.com>
2025-12-22 13:07:15 +0530

    deleted slug

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,44 @@
 import Image from "next/image";
 import HomeSection from "./components/home/HomeSection";
+import Hero from "./components/home/Hero";
+import AboutSection from "./components/home/AboutSection";
+import Category from "./components/home/Category";
+import CTASection from "./components/home/CTASection";
+import FeaturedCollections from "./components/home/FeaturedCollections";
+import Testimonials from "./components/home/Testimonials";
+import Jewellery from "./components/home/Jewellery";
 
 export default function Home() {
   return (
-   <HomeSection/>
+  <>
+   {/* <HomeSection/> */}
+
+
+   <Hero />                 
+     {/* // Eye-catching first impression */}
+
+<FeaturedCollections />   
+ {/* // Latest or premium collections */}
+
+<Category />               
+ {/* // Allow user to filter/explore by type */}
+
+<Jewellery />               
+{/* // Gold/Diamond card showcase */}
+
+<AboutSection />           
+ {/* // Brand story, trust, team */}
+
+<Testimonials />           
+ {/* // Social proof */}
+
+<CTASection />              
+{/* // Conversion / newsletter / shop now */}
+
+
+   </>
+
+
  
   );
 }
