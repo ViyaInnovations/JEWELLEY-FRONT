ec6ea5712cc3e3265ce7bf6b9dec44120d51c864
dilaksh06 <kamalathasandilakshan@gmail.com>
2025-12-23 21:30:42 +0530

    jewellery secton test

--- app/page.tsx
+++ app/page.tsx
@@ -32,7 +32,7 @@ export default function Home() {
  
 {/* <ProductGrid/> */}
 
-{/* <Jewellery />                */}
+<Jewellery />               
 {/* // Gold/Diamond card showcase */}
 
 {/* <AboutSection />            */}
