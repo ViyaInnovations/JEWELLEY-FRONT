1e04a4efc6245a8cba8d7f69456c75784b230e5d
dilaksh06 <kamalathasandilakshan@gmail.com>
2025-12-23 21:14:59 +0530

    dynamic page

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,21 +1,22 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import { useSearchParams } from "next/navigation";
 import products from "../data/products.json";
 import ProductCard from "../components/home/ProductCard";
 import type { Product } from "../types/product";
 
-export default function page() {
+export default function Page() {
   const searchParams = useSearchParams();
   const category = searchParams.get("category");
-  console.log( category)
 
   const filteredProducts = category
     ? (products as Product[]).filter(
         (product) => product.category === category
       )
     : (products as Product[]);
- console.log ('filered products', filteredProducts)
+
   return (
     <section className="max-w-7xl mx-auto px-4 py-12">
       <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-6">
